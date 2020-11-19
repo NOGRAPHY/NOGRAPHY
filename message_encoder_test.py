c94d5fe1f241c3347f3eccbd53c8077f7220af8a
Ekrem <ekrem.emre@posteo.de>
2020-11-19 15:23:29 +0100

    added test for message encoder

---
+++ message_encoder_test.py
@@ -0,0 +1,19 @@
+import string
+import random
+
+from message_encoder import encode, decode
+
+printable_ascii_string = list(string.printable)
+
+for n in range(2, 33):
+    # print("base", n)
+    random.shuffle(printable_ascii_string)
+    msg = ''.join(printable_ascii_string)
+
+    assert msg == decode(encode(msg, n))
+
+
+custom_msg = "https://www.htwg-konstanz.de/hochschule/fakultaeten/informatik/uebersicht/"
+custom_base = 3
+
+assert custom_msg == decode(encode(custom_msg, custom_base))
