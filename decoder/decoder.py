d0e92e4eea70788e7cd3a0d23bd1fcc24d4349f2
Robert Nickel <robert.nickel@aikq.de>
2020-12-10 14:57:36 +0100

    Structered encoder and decoder in separate packages. Structered unit tests for embedder, encoder and decoder properly

--- decoder/decoder.py
+++ decoder/decoder.py
@@ -1,17 +1,5 @@
-def encode(plain_string, base):
-    bit_string = ''.join([format(c, 'b').zfill(8) for c in plain_string.encode('utf8')])
-
-    # split bit_string into chunks the length of base
-    chunks = [bit_string[::-1][i:i + base] for i in range(0, len(bit_string), base)]
-
-    # reverse list of chunks AND every chunk itself.
-    chunks = [chunk[::-1].zfill(base) for chunk in chunks[::-1]]
-
-    return chunks
-
-
-def decode(coded_string):
-    bit_string = ''.join(coded_string)
+def decode(encoded_string):
+    bit_string = ''.join(encoded_string)
 
     # split bit_string into chunks the length of 8
     chunks = [bit_string[::-1][i:i + 8] for i in range(0, len(bit_string), 8)]
