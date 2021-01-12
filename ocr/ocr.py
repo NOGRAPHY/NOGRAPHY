12817082a4484fbe09effc5f42bd20a60c03edb1
Robert Nickel <robert.nickel@aikq.de>
2021-01-12 09:54:44 +0100

    Update ocr.py

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -8,17 +8,18 @@ def ocr():
     hImg, wImg = img.shape
     return pytesseract.image_to_boxes(img)
 
-img = cv2.imread('ocr/example/test.png')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-hImg, wImg = img.shape
-boxes = pytesseract.image_to_boxes(img)
+def other():
+    img = cv2.imread('ocr/example/test.png')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hImg, wImg = img.shape
+    boxes = pytesseract.image_to_boxes(img)
 
-print(boxes)
+    print(boxes)
 
-for b in boxes.splitlines():
-    b = b.split(' ')
-    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-    cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (0, 0, 255), 1)
-cv2.imshow('Result', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    for b in boxes.splitlines():
+        b = b.split(' ')
+        x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+        cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (0, 0, 255), 1)
+    cv2.imshow('Result', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
