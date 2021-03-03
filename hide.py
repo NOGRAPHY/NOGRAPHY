86604d1be681dd06d9da3368337455c378744b63
Robert Nickel <robert.nickel@aikq.de>
2021-03-03 07:07:13 +0100

    return the pdf document

--- hide.py
+++ hide.py
@@ -5,19 +5,21 @@ from embedder import embedder
 app = Flask(__name__)
 
 placeholder = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+secret_max_length = 50
+
 
 @app.route('/')
 def index():
-    message = request.args.get('message', '').strip()
-    if len(message) > 10:
-        return render_template('hide.html', error = 'That message is to long. Please enter no more than 10 characters.')
-
-    embedder.generate_document(
-        embedder.embed(
-            embedder.setup_document(),
-            placeholder,
-            encoder.encode(message, 3)), 'document with hidden stuff')
-    
+    secret = request.args.get('secret', '').strip()
+    if len(secret) > secret_max_length:
+        return render_template('hide.html', error = 'Secret is too long. Enter no more than ' + str(secret_max_length)+ ' characters.')
+    elif len(secret) > 0:
+        embedder.generate_document(
+            embedder.embed(
+                embedder.setup_document(),
+                placeholder,
+                encoder.encode(secret, 3)), 'hidden_secret')
+        return send_file('hidden_secret.pdf', as_attachment=True)
     return render_template('hide.html')
 
 if __name__ == "__main__":
