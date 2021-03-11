250151fb94dfc84ab18b936b73fe7a72af54c611
ekrem <ekrem.emre@posteo.de>
2021-03-11 12:55:25 +0100

    added single CNN model

---
+++ cnn/single_model/cnn_single_model.py
@@ -0,0 +1,57 @@
+import os
+
+from tensorflow import keras
+import numpy as np
+
+from embedder import embedder
+
+
+class SingleModel:
+    MODEL_FILNENAME = "model-20210309-1356.h5"
+
+    LABELS = {
+        0: (7, embedder.DUMMY_CODEBOOK[7]),  # Charter
+        1: (6, embedder.DUMMY_CODEBOOK[6]),  # Clara
+        2: (4, embedder.DUMMY_CODEBOOK[4]),  # Fourier
+        3: (2, embedder.DUMMY_CODEBOOK[2]),  # GyreBonum
+        4: (1, embedder.DUMMY_CODEBOOK[1]),  # GyrePagella
+        5: (3, embedder.DUMMY_CODEBOOK[3]),  # GyreSchola
+        6: (0, embedder.DUMMY_CODEBOOK[0]),  # GyreTermes
+        7: (9, {'font': 'lmr', 'color': 'black', 'fontname': "Latin Modern Times"}),    # LatinModernTimes
+        8: (5, embedder.DUMMY_CODEBOOK[5]),  # Tinos
+    }
+
+    DEFAULT_FONT = 9
+
+    def __init__(self):
+        model_path = os.path.join(os.path.split(os.path.realpath(__file__))[0], self.MODEL_FILNENAME)
+
+        self.model = keras.models.load_model(model_path)
+
+    def predict(self, images):
+        if not isinstance(images, np.ndarray):
+            raise TypeError("predict() argument 'img_array' must be " +
+                            str(type(np.ndarray([]))) + ", not " + str(type(images)))
+
+        # Normalize image, if it is not yet done.
+        if np.max(images) != 1.0:
+            images = images / np.max(images)
+
+        predictions = self.model.predict(images)
+
+        font_indexes = []
+        font_info = []
+
+        for prediction in predictions:
+            argmax = np.argmax(prediction).item()
+
+            font_index, info = self.LABELS[argmax]
+
+            # As soon the default font is recognized, the secret message was completely read.
+            if font_index == self.DEFAULT_FONT:
+                break
+
+            font_indexes.append(font_index)
+            font_info.append(info)
+
+        return font_indexes, font_info
---
+++ cnn/single_model/test_cnn_single_model.py
@@ -0,0 +1,28 @@
+from cnn.single_model.cnn_single_model import SingleModel
+
+import os
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+
+def test_single_model():
+    image_paths = sorted(os.listdir("example"))
+
+    assert len(image_paths) == 35  # We have 35 images.
+
+    imgs = []
+    for imagePath in image_paths:
+        img_path = os.path.join("example", imagePath)
+        img = load_img(img_path, color_mode="grayscale", target_size=(200, 200))
+        img_array = img_to_array(img)
+        imgs.append(img_array)
+
+    imgs = np.array(imgs)
+
+    model = SingleModel()
+    font_indexes, _ = model.predict(imgs)
+
+    assert len(font_indexes) == 30  # We have 30 relevant glyphs.
+
+    expected = [4, 4, 1, 6, 6, 4, 4, 3, 7, 5, 5, 3, 5, 0, 0, 1, 7, 5, 1, 6, 6, 0, 3, 3, 5, 4, 1, 4, 4, 0]
+    assert font_indexes == expected
