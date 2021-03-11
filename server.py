9061851de29c10b6d89ebc2b0139d0bde6db9620
ekrem <ekrem.emre@posteo.de>
2021-03-11 14:31:59 +0100

    added ocr and cnn prediction to server exposing-function

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 import os
 import uuid
+import secrets
 
 from flask import Flask, request, redirect, render_template, send_file, flash
-import secrets
 from pdf2image import convert_from_path
 from werkzeug.utils import secure_filename
 
-from encoder import encoder
 from embedder import embedder
+from encoder import encoder
+from decoder import decoder
+from ocr import ocr
+from cnn.single_model.cnn_single_model import SingleModel
+
 
 SERVER_TMP = os.path.join(os.path.split(os.path.realpath(__file__))[0], "server_tmp")
 UPLOAD_TMP = os.path.join(SERVER_TMP, "upload")
 os.makedirs(UPLOAD_TMP, exist_ok=True)
+
+ENCODING_DECODING_BASE = 3
+
+cnn_model = SingleModel()
 
 app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe(32)
@@ -43,7 +51,7 @@ def hide():
                 document=embedder.embed(
                     embedder.setup_document(),
                     placeholder,
-                    encoder.encode(secret, 3)
+                    encoder.encode(secret, ENCODING_DECODING_BASE)
                 ),
                 file_name=file_pdf
             )
@@ -62,16 +70,26 @@ def expose():
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No or empty file uploaded.')
-
             return render_template('expose.html')
 
         file = request.files['file']
         filename = os.path.splitext(secure_filename(file.filename))
         filename = "{}_{}{}".format(filename[0], uuid.uuid4(), filename[1])
 
-        file.save(os.path.join(UPLOAD_TMP, filename))
+        filepath = os.path.join(UPLOAD_TMP, filename)
+        file.save(filepath)
 
-        return render_template('expose.html')
+        try:
+            characters, boxes = ocr.recognizeCharacters(filepath)
+            glyphs = ocr.createLetterImages(characters, boxes, filepath, 200, save_files=False)
+        except IndexError as exception:
+            flash(exception)
+            return render_template('expose.html')
+
+        font_indexes, _ = cnn_model.predict(glyphs)
+        exposed_message = decoder.decode_from_font_indexes(font_indexes, ENCODING_DECODING_BASE)
+
+        return render_template('expose.html', exposed_message=exposed_message)
 
 
 if __name__ == "__main__":
