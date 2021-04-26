880273090cedaf99c027a98e890cdcf7493bbad8
Ekrem <ekrem.emre@posteo.de>
2021-04-26 20:12:27 +0200

    added OCR to glyph perturber

--- glyph_perturber/main.py
+++ glyph_perturber/main.py
@@ -4,15 +4,21 @@ import shutil
 import string
 import random
 import argparse
+import tempfile
+
+from ocr import ocr
 
 from tqdm import tqdm
-from imgaug import parameters as iap
 from fontTools.ttLib import TTFont
+from imgaug import parameters as iap
+from pdf2image import convert_from_path
 from pylatex.base_classes import Command
 from pylatex import Document, Package, NoEscape
 
 
 class GlyphPerturber:
+    characters = list(string.ascii_letters) # + ["adieresis", "odieresis", "udieresis", "Adieresis", "Odieresis", "Udieresis"]
+
     @staticmethod
     def rename_font(_font, number):
         old_name = ""
@@ -40,10 +46,9 @@ class GlyphPerturber:
 
     @staticmethod
     def perturb_glyphs(_font, number_of_points):
-        chars = list(string.ascii_letters) + ["adieresis", "odieresis", "udieresis", "Adieresis", "Odieresis", "Udieresis"]
         poisson_distribution = iap.RandomSign(iap.Poisson(5))
 
-        for char in chars:
+        for char in GlyphPerturber.characters:
             if char in font.getGlyphSet():
                 glyph = _font.getGlyphSet().get(char)
 
@@ -61,14 +66,14 @@ class GlyphPerturber:
 
 class PdfGenerator:
     @staticmethod
-    def setup_document():
+    def setup_document(letterspace=25):
         document = Document(document_options='a4paper', geometry_options={'left': '10mm', 'top': '10mm'},
                             lmodern=False, inputenc=None,
                             page_numbers=False, indent=False)
 
         # Add Font integration and glyph spacing.
         document.packages.append(Package('fontspec'))
-        document.packages.append(Command('defaultfontfeatures', arguments="LetterSpace=25.0"))
+        document.packages.append(Command('defaultfontfeatures', arguments=f"LetterSpace={letterspace}"))
 
         return document
 
@@ -78,26 +83,59 @@ class PdfGenerator:
 
         for perturbed_font in perturbed_fonts:
             document.append(Command('setmainfont', arguments=NoEscape(perturbed_font)))
-            document.append("abcdefghijklmnopqrstuvwxyzäöüABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ")
+            document.append(string.ascii_lowercase + string.ascii_uppercase)
             document.append("\n")
 
         return document
 
     @staticmethod
     def generate_training_pdf(perturbed_font):
-        document = PdfGenerator.setup_document()
+        document = PdfGenerator.setup_document(letterspace=15)
 
         document.append(Command('setmainfont', arguments=NoEscape(perturbed_font)))
-        document.append("abcdefghijklmnopqrstuvwxyzäöü")
+        document.append(string.ascii_lowercase)
         document.append("\n")
-        document.append("ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ")
+        document.append("\n")
+        document.append(string.ascii_uppercase)
         document.append("\n")
 
         return document
 
 
+class GlyphExtractor:
+    @staticmethod
+    def extract_glyphs(font_pdf_path, dpis):
+        for dpi in dpis:
+            filename = os.path.splitext(os.path.basename(font_pdf_path))[0]
+            new_pdf_dir = os.path.join(os.path.dirname(font_pdf_path), f"{dpi}dpi", filename)
+            os.makedirs(new_pdf_dir, exist_ok=True)
+
+            new_pdf_path = os.path.join(new_pdf_dir, os.path.basename(font_pdf_path))
+
+            # copy PDF of font into its own directory
+            shutil.copy(font_pdf_path, new_pdf_path)
+
+            # convert PDF to PNG
+            png_path = os.path.join(new_pdf_dir, f"{filename}.png")
+            with tempfile.TemporaryDirectory() as path:
+                images = convert_from_path(new_pdf_path, dpi=int(dpi), output_folder=path, fmt="png")[0]
+                images.save(png_path)
+
+            characters, boxes = ocr.recognizeCharacters(png_path)
+
+            if len(GlyphPerturber.characters) != len(boxes):
+                raise RuntimeError("OCR was not able to recognize all glyphs.")
+
+            ocr.createLetterImages(characters, boxes, png_path, 200, save_files=True)
+
+            os.remove(new_pdf_path)
+            os.remove(png_path)
+
+        os.remove(font_pdf_path)
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Perturbs lower and upper ASCII and umlaut (öäü) glyphs of a given font file.")
+    parser = argparse.ArgumentParser(description="Perturbs glyphs of a given font file.")
     parser.add_argument("-f", "--font", type=str, required=True, help="Font file to be perturbed.")
     parser.add_argument("-o", "--output", type=str, required=True, help="Directory where all perturbed fonts will be saved.")
     parser.add_argument("-n", "--number", type=int, required=True, help="Number of perturbed fonts.")
@@ -105,6 +143,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--preview", action="store_true", help="Generate a preview PDF file with all perturbed glyphs.")
     parser.add_argument("--train", action="store_true", help="Generate image data for training with all perturbed glyphs.")
+    parser.add_argument("--dpis", nargs='+', default=300)
 
     args = parser.parse_args()
 
@@ -146,6 +185,9 @@ if __name__ == "__main__":
 
             shutil.move(os.path.join(args.output, f"{train_pdf_filename}.pdf"),
                         os.path.join(train_path, f"{train_pdf_filename}.pdf"))
+
+            GlyphExtractor.extract_glyphs(font_pdf_path=os.path.join(train_path, f"{train_pdf_filename}.pdf"),
+                                          dpis=[] + args.dpis)
 
     if args.preview:
         PdfGenerator.generate_preview(perturbed_font_names).generate_pdf(
