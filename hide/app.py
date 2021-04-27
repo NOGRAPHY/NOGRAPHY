cb6abaa3983e12ec7705e0d54fa02970e8696d0d
Robert Nickel <robert.nickel@aikq.de>
2021-04-27 16:17:42 +0200

    Added failure handling if secret is too long for placeholder text. Deleted all unnecessary comments and formatted code.

--- hide/app.py
+++ hide/app.py
@@ -8,47 +8,38 @@ from io import BytesIO
 import string
 
 FONT_SIZE = 18
+ENCODING_DECODING_BASE = 3
+
 
 def lambda_handler(event, context):
     placeholder = "In a hole in the ground there lived a hobbit. Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort."
     secret = "LOTR is better than GOT"
-    encoded_secret = [int(c, 2) for c in encoder.encode(secret)]
+    # TODO: get parameters from request.
 
-    image = Image.new("RGB", (500, 200), 'white')
+    if len(secret) * ENCODING_DECODING_BASE > len(placeholder):
+        return {
+            "statusCode": 400,
+            "error": "Secret is too long. Make it shorter or the placeholder longer."
+        }
+    else:
+        encoded_secret = [int(c, 2) for c in encoder.encode(
+            secret, ENCODING_DECODING_BASE)]
+        image = Image.new("RGB", (500, 200), 'white')
+        letters_and_fonts = get_letters_and_fonts(placeholder, encoded_secret)
 
-    letters_and_fonts = get_letters_and_fonts(placeholder, encoded_secret)
+        fit_text(image, letters_and_fonts, 'black', margin=50)
 
-    fit_text(image, letters_and_fonts, 'black', margin=50)
-
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    image_str = base64.b64encode(buffered.getvalue())
-    return {
-        "statusCode": 200,
-        "headers": {
-            'Content-Type': 'image/png'
-        },
-        "body": image_str,
-        "isBase64Encoded": True
-    }
-
-    #placeholder = event['placeholder'].strip()
-    #show_colors = event['show_colors']
-
-    # if len(secret) * ENCODING_DECODING_BASE > len(placeholder):
-    #    return {
-    #        "statusCode": 400,
-    #        "error": "Secret is too long. Make it shorter or the placeholder longer."
-    #    }
-    # else:
-
-    # encoded_secret = encoder.encode(secret, ENCODING_DECODING_BASE)
-    # document = embedder.embed(
-    #                 embedder.setup_document(),
-    #                 placeholder,
-    #                 encoded_secret,
-    #                 show_colors
-    #             )
+        buffered = BytesIO()
+        image.save(buffered, format="PNG")
+        image_str = base64.b64encode(buffered.getvalue())
+        return {
+            "statusCode": 200,
+            "headers": {
+                'Content-Type': 'image/png'
+            },
+            "body": image_str,
+            "isBase64Encoded": True
+        }
 
 
 def get_letters_and_fonts(placeholder, encoded_secret):
@@ -71,10 +62,6 @@ def get_letters_and_fonts(placeholder, encoded_secret):
 
 # based on https://stackoverflow.com/questions/58041361/break-long-drawn-text-to-multiple-lines-with-pillow
 def break_into_lines(letters_and_fonts, width, font, draw):
-
-# letters = ''.join([i[0] for i in letters_and_fonts])
-
-
     if not letters_and_fonts:
         return
     lo = 0
@@ -97,7 +84,8 @@ def fit_text(img, letters_and_fonts, color, margin):
     width = img.size[0] - 2 - margin
     draw = ImageDraw.Draw(img)
     measure_font = ImageFont.truetype('./assets/0.ttf', FONT_SIZE)
-    pieces = list(break_into_lines(letters_and_fonts, width, measure_font, draw))
+    pieces = list(break_into_lines(
+        letters_and_fonts, width, measure_font, draw))
     height = sum(p[2] for p in pieces)
     if height > img.size[1]:
         raise ValueError("text doesn't fit")
@@ -105,7 +93,8 @@ def fit_text(img, letters_and_fonts, color, margin):
     for t, w, h in pieces:
         x = (img.size[0] - w) // 2
         for letter, font in t:
+            font = ImageFont.truetype('./assets/'+str(font)+'.ttf', FONT_SIZE)
             draw.text((x, y), letter,
-                      font=ImageFont.truetype('./assets/'+str(font)+'.ttf', FONT_SIZE), fill=color)
-            x = x + draw.textsize(letter)[0]
+                      font=font, fill=color)
+            x = x + draw.textsize(letter, font=font)[0]
         y += h
