b27bc801f16bbc90adfdfe02466b3143a2db1cec
Ekrem <ekrem.emre@posteo.de>
2021-08-10 09:11:44 +0200

    updated CNN-Model tester.

--- expose/app/cnn/test_glyph_recognizer.py
+++ expose/app/cnn/test_glyph_recognizer.py
@@ -2,27 +2,25 @@ from glyph_recognizer import GlyphRecognizer
 
 import os
 import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import cv2
 
 
-def test_single_model():
+def test_glyph_recognizer():
     image_paths = sorted(os.listdir("example"))
 
     assert len(image_paths) == 35  # We have 35 images.
 
-    imgs = []
-    for imagePath in image_paths:
-        img_path = os.path.join("example", imagePath)
-        img = load_img(img_path, color_mode="grayscale", target_size=(200, 200))
-        img_array = img_to_array(img)
-        imgs.append(img_array)
+    glyph_images = []
+    for image_path in image_paths:
+        glyph_images.append(cv2.imread(os.path.join("example", image_path)))
 
-    imgs = np.array(imgs)
+    glyph_images = np.array(glyph_images)
 
     model = GlyphRecognizer()
-    font_indexes, _, _ = model.predict(imgs)
+    font_indexes, _ = model.predict(glyph_images)
 
     assert len(font_indexes) == 30  # We have 30 relevant glyphs.
 
-    expected = [4, 4, 1, 6, 6, 4, 4, 3, 7, 5, 5, 3, 5, 0, 0, 1, 7, 5, 1, 6, 6, 0, 3, 3, 5, 4, 1, 4, 4, 0]
+    expected = [0, 4, 4, 1, 4, 5, 3, 3, 0, 6, 6, 1, 5, 7, 1, 0, 0, 5, 3, 5, 5, 7, 3, 4, 4, 6, 6, 1, 4, 4]
+
     assert font_indexes == expected
