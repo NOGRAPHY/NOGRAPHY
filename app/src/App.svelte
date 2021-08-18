1ad3d86f6cfd2e5c78c587acd5e2566454340948
Robert Nickel <robert.nickel@aikq.de>
2021-08-18 10:52:55 +0200

    Changed default value for secret to contain symbols, since that is supported now.

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -3,7 +3,7 @@
 	import ValidationError from "./ValidationError.svelte";
 
 	let secret =
-		"Aragorn broke two of his toes while kicking an Uruk Hai helmet";
+		"Aragorn broke two of his toes while kicking an Uruk-Hai helmet.";
 	let dummy =
 		"In a hole in the ground there lived a hobbit. Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort. It had a perfectly round door like a porthole, painted green, with a shiny yellow brass knob in the exact middle. The door opened on to a tube-shaped hall like a tunnel: a very comfortable tunnel without smoke, with panelled walls, and floors tiled and carpeted, provided with polished chairs, and lots and lots of pegs for hats and coats - the hobbit was fond of visitors.";
 	let loading = false;
